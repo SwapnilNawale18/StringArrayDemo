52ab072085ced27b10664db59c545b3d621046c5
Swapnil Nawale <swapnilnawale18@gmail.com>
2021-08-28 21:21:31 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace StringArrayDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] str = new String[] {"Sunflower", "Rose", "Jasmine", "Marigold", "Lily", "Lotus"};
+            // copying an array
+            String[] Info = str;
+            foreach (string k in Info)
+            {
+                Console.WriteLine(k[2]);                // student output :- Jasmine
+            }
+        }
+    }
+}
+/*
+ * It prints 3rd charecter of every string
+ */
